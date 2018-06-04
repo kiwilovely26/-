7a78f7cf86576340ffa399246e42baf8b2bdca38
kiwilovely26 <kiwi_loveli@126.com>
2018-06-04 17:04:25 +0800

    Add files via upload

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,12 +3,52 @@
 const app = getApp()
 Page({
   data: {
+    swingShow: false,
+    zoomInShow:false,
+    slideInDownShow:false
   },
-  onLoad: function () {    
+  onLoad: function () {
+
+  }, 
+  onReady:function(){
+    
   },
-  bindViewHome: function(){
-    wx.navigateTo({
-      url: '../home/home'
+  onShow:function(){
+    let that = this;
+    setTimeout(function(){
+      that.setData({
+        swingShow:true,
+        zoomInShow: true,
+        slideInDownShow: true
+      })
+    },0)
+  },
+  onHide:function(){
+    console.log("hide");
+    this.setData({
+      swingShow: false,
+      zoomInShow: false,
+      slideInDownShow: false
     })
+  },
+  onShareAppMessage: function (res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log("来自页面内转发按钮"); 
+      console.log(res.target)
+    }else{
+      console.log("来自右上角转发菜单");
+    }
+    return {
+      title: '儿童节快乐～',
+      path: '/pages/index/index',
+      imageUrl:"/assets/img/children-home.png",
+      success:(res)=>{
+        console.log("转发成功",res)
+      },
+      fail:(res)=>{
+        console.log("转发失败",res)
+      }
+    }
   }
 })
